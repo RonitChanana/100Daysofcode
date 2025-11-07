49b712f879a07fc239a95f03e0c52ce969241066
RonitChanana <Ronit.26515@stu.upes.ac.in>
2025-11-07 11:44:53 +0530

    Update day2q1.c

--- day2q1.c
+++ day2q1.c
@@ -1,3 +1,5 @@
+Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
 #include <stdio.h>
 
 int main() {
@@ -14,3 +16,4 @@ int main() {
     
     return 0;
 }
+
